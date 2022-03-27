8675638c5737f8304f2584cd1b950332a64d1d1b
Vexirale <73032255+Vexirale@users.noreply.github.com>
2022-03-27 12:17:57 +0200

    Update clientInfo.ts

--- backend/clientInfo.ts
+++ backend/clientInfo.ts
@@ -2,8 +2,8 @@ import { Socket } from "socket.io"
 
 export default class ClientInfo {
   public latency = 0
-  public name = "vexirale"
-  public isHost = true
+  public name = "Unnamed"
+  public isHost = false
   public loggedIn = false
   public trackUri = ""
 
