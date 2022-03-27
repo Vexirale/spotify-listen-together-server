7083782a494d640fafd9241ac5b319b4e298f3fa
Vexirale <73032255+Vexirale@users.noreply.github.com>
2022-03-27 11:50:04 +0200

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css'
 const Header = () => {
   return (
     <div className={styles.mainTextContainer}>
-      <div className={styles.mainText}>Spotify <span className={styles.specialText}>Listen Together</span></div>
+      <div className={styles.mainText}>Listen with <span className={styles.specialText}>vexirale :D</span></div>
     </div>
   )
 }
